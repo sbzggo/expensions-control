0d7dc49bd0b5bccec35347ea20c2049781d45a42
Guilherme Oliveira <goncalvesguilherme741@gmail.com>
2026-02-10 17:03:43 -0300

    wip: validação inicial do formulário de login

---
+++ index.js
@@ -0,0 +1,66 @@
+console.log("JS carregou");
+
+
+function onChangeEmail() {
+       toggleButtonsDisable();
+       toggleEmailErrors();
+    }  
+
+    function onChangePassword() {
+        toggleButtonsDisable();
+        togglePasswordErrors();
+    }
+
+    function isEmailValid() {
+        const email = document.getElementById("email").value;
+        if (!email) {
+            return false;
+        }
+        return validateEmail(email);
+    }
+
+    function toggleEmailErrors() {
+        const email = document.getElementById("email").value;
+        if(!email) {
+            document.getElementById('email-required-error').style.display = "block";
+        }   else {
+            document.getElementById('email-required-error').style.display = "none";
+        }
+
+        if (validateEmail(email)){
+            document.getElementById('email-invalid-error').style.display = "none";
+        } else {
+            document.getElementById('email-invalid-error').style.display = "block";
+        }
+    }
+
+    function togglePasswordErrors() {
+        const password = document.getElementById('password').value;
+        if (!password) {
+            document.getElementById('password-required-error').style.display = "block";        
+        } else {
+            document.getElementById('password-required-error').style.display = "none";
+
+        }
+    }
+
+    function toggleButtonsDisable() {
+         const emailValid = isEmailValid();
+        document.getElementById('recover-password-buton').disabled = !emailValid;
+
+        const passwordValid = isPasswordValid();
+        document.getElementById('login-button').disabled = !emailValid || !passwordValid;
+        //pegar o valor da senha
+    }
+
+    function isPasswordValid() {
+    const password = document.getElementById("password").value;
+    return password.length >= 1;
+}
+
+
+
+    function validateEmail(email) {
+        return /\S+@\S+\.\S+/.test(email);
+    }
+
